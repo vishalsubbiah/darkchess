30ae6ef129ee9406c5679c88825f9bee4f7a7ccf
Vishal Subbiah <subbiahvishal@gmail.com>
2019-02-24 00:48:14 -0800

    knight moves done, bug in some moves for pawn, maybe for others, refer issue #13

--- src/knight.py
+++ src/knight.py
@@ -19,9 +19,70 @@ class Knight(Piece):
     def get_moves(self, board):
         moves=[]
         pos = self.get_position()
+        x,y = pos
         if self.team == "white":
+            if x-2>=0 and y-1>=0:
+                if board[x-2,y-1].team is not "white":
+                    moves.append([pos,[x-2,y-1],"base"])
+
+            if x-2>=0 and y+1<=7:
+                if board[x-2,y+1].team is not "white":
+                    moves.append([pos,[x-2,y+1],"base"])
+
+            if x-1>=0 and y-2>=0:
+                if board[x-1,y-2].team is not "white":
+                    moves.append([pos,[x-1,y-2],"base"])
+
+            if x+1<=7 and y-2>=0:
+                if board[x+1,y-2].team is not "white":
+                    moves.append([pos,[x+1,y-2],"base"])
+            if x+2<=7 and y-1>=0:
+                if board[x+2,y-1].team is not "white":
+                    moves.append([pos,[x+2,y-1],"base"])
+
+            if x+2<=7 and y+1<=7:
+                if board[x+2,y+1].team is not "white":
+                    moves.append([pos,[x+2,y+1],"base"])
+
+            if x+1<=7 and y+2<=7:
+                if board[x+1,y+2].team is not "white":
+                    moves.append([pos,[x+1,y+2],"base"])
+            if x-1>=0 and y+2<=7:
+                if board[x-1,y+2].team is not "white":
+                    moves.append([pos,[x-1,y+2],"base"])
+
             return moves
         elif self.team == "black":
+            if x-2>=0 and y-1>=0:
+                if board[x-2,y-1].team is not "black":
+                    moves.append([pos,[x-2,y-1],"base"])
+
+            if x-2>=0 and y+1<=7:
+                if board[x-2,y+1].team is not "black":
+                    moves.append([pos,[x-2,y+1],"base"])
+
+            if x-1>=0 and y-2>=0:
+                if board[x-1,y-2].team is not "black":
+                    moves.append([pos,[x-1,y-2],"base"])
+
+            if x+1<=7 and y-2>=0:
+                if board[x+1,y-2].team is not "black":
+                    moves.append([pos,[x+1,y-2],"base"])
+            if x+2<=7 and y-1>=0:
+                if board[x+2,y-1].team is not "black":
+                    moves.append([pos,[x+2,y-1],"base"])
+
+            if x+2<=7 and y+1<=7:
+                if board[x+2,y+1].team is not "black":
+                    moves.append([pos,[x+2,y+1],"base"])
+
+            if x+1<=7 and y+2<=7:
+                if board[x+1,y+2].team is not "black":
+                    moves.append([pos,[x+1,y+2],"base"])
+            if x-1>=0 and y+2<=7:
+                if board[x-1,y+2].team is not "black":
+                    moves.append([pos,[x-1,y+2],"base"])
+
             return moves
         else:
             raise ValueError("this team:"+self.team+" doesn't exist in the realm of this game")
