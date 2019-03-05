176838d3a0911ff8913bcf14b732d3424baa1c94
Vishal Subbiah <subbiahvishal@gmail.com>
2019-03-04 20:12:31 -0800

    list to tuples

--- src/queen.py
+++ src/queen.py
@@ -24,71 +24,71 @@ class Queen(Piece):
             # rook moves
             for j in range(y+1,8):
                 if board[x,j].team == None:
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                 elif board[x,j].team == "black":
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                     break
                 else:
                     break
             for j in range(y-1,-1,-1):
                 if board[x,j].team == None:
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                 elif board[x,j].team == "black":
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                     break
                 else:
                     break
             for i in range(x+1,8):
                 if board[i,y].team == None:
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                 elif board[i,y].team == "black":
                     print("here")
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                     break
                 else:
                     break
             for i in range(x-1,-1,-1):
                 if board[i,y].team == None:
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                 elif board[i,y].team == "black":
                     print("here")
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                     break
                 else:
                     break
             # bishop moves
             for i in range(min(x,y)):
                 if board[x-i-1,y-i-1].team == None:
-                    moves.append([pos,[x-i-1,y-i-1],'base'])
+                    moves.append([pos,(x-i-1,y-i-1),'base'])
                 elif board[x-i-1,y-i-1].team == "black":
-                    moves.append([pos,[x-i-1,y-i-1],'base'])
+                    moves.append([pos,(x-i-1,y-i-1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(7-x,7-y)):
                 if board[x+i+1,y+i+1].team == None:
-                    moves.append([pos,[x+i+1,y+i+1],'base'])
+                    moves.append([pos,(x+i+1,y+i+1),'base'])
                 elif board[x+i+1,y+i+1].team == "black":
-                    moves.append([pos,[x+i+1,y+i+1],'base'])
+                    moves.append([pos,(x+i+1,y+i+1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(x,7-y)):
                 if board[x-i-1,y+i+1].team == None:
-                    moves.append([pos,[x-i-1,y+i+1],'base'])
+                    moves.append([pos,(x-i-1,y+i+1),'base'])
                 elif board[x-i-1,y+i+1].team == "black":
-                    moves.append([pos,[x-i-1,y+i+1],'base'])
+                    moves.append([pos,(x-i-1,y+i+1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(7-x,y)):
                 if board[x+i+1,y-i-1].team == None:
-                    moves.append([pos,[x+i+1,y-i-1],'base'])
+                    moves.append([pos,(x+i+1,y-i-1),'base'])
                 elif board[x+i+1,y-i-1].team == "black":
-                    moves.append([pos,[x+i+1,y-i-1],'base'])
+                    moves.append([pos,(x+i+1,y-i-1),'base'])
                     break
                 else:
                     break
@@ -97,69 +97,69 @@ class Queen(Piece):
             # rook moves
             for j in range(y+1,8):
                 if board[x,j].team == None:
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                 elif board[x,j].team == "white":
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                     break
                 else:
                     break
             for j in range(y-1,-1,-1):
                 if board[x,j].team == None:
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                 elif board[x,j].team == "white":
-                    moves.append([pos,[x,j],'base'])
+                    moves.append([pos,(x,j),'base'])
                     break
                 else:
                     break
             for i in range(x+1,8):
                 if board[i,y].team == None:
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                 elif board[i,y].team == "white":
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                     break
                 else:
                     break
             for i in range(x-1,-1,-1):
                 if board[i,y].team == None:
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                 elif board[i,y].team == "white":
-                    moves.append([pos,[i,y],'base'])
+                    moves.append([pos,(i,y),'base'])
                     break
                 else:
                     break
             #bishop moves
             for i in range(min(x,y)):
                 if board[x-i-1,y-i-1].team == None:
-                    moves.append([pos,[x-i-1,y-i-1],'base'])
+                    moves.append([pos,(x-i-1,y-i-1),'base'])
                 elif board[x-i-1,y-i-1].team == "white":
-                    moves.append([pos,[x-i-1,y-i-1],'base'])
+                    moves.append([pos,(x-i-1,y-i-1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(7-x,7-y)):
                 if board[x+i+1,y+i+1].team == None:
-                    moves.append([pos,[x+i+1,y+i+1],'base'])
+                    moves.append([pos,(x+i+1,y+i+1),'base'])
                 elif board[x+i+1,y+i+1].team == "white":
-                    moves.append([pos,[x+i+1,y+i+1],'base'])
+                    moves.append([pos,(x+i+1,y+i+1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(x,7-y)):
                 if board[x-i-1,y+i+1].team == None:
-                    moves.append([pos,[x-i-1,y+i+1],'base'])
+                    moves.append([pos,(x-i-1,y+i+1),'base'])
                 elif board[x-i-1,y+i+1].team == "white":
-                    moves.append([pos,[x-i-1,y+i+1],'base'])
+                    moves.append([pos,(x-i-1,y+i+1),'base'])
                     break
                 else:
                     break
 
             for i in range(min(7-x,y)):
                 if board[x+i+1,y-i-1].team == None:
-                    moves.append([pos,[x+i+1,y-i-1],'base'])
+                    moves.append([pos,(x+i+1,y-i-1),'base'])
                 elif board[x+i+1,y-i-1].team == "white":
-                    moves.append([pos,[x+i+1,y-i-1],'base'])
+                    moves.append([pos,(x+i+1,y-i-1),'base'])
                     break
                 else:
                     break
